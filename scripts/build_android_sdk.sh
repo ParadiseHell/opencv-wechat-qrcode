12e0d17bb684a02c7a7cc1bb46d6418438dbe57d
chengtao05 <chengtao05@kuaishou.com>
2021-05-13 15:25:27 +0800

    refactor: update script

--- scripts/build_android_sdk.sh
+++ scripts/build_android_sdk.sh
@@ -1,13 +1,21 @@
 #!/bin/sh
 
-#git clone -b 4.5.2 https://github.com/opencv/opencv.git
+echo "Clone opencv"
+git clone -b 4.5.2 https://github.com/opencv/opencv.git >/dev/null
 
-git clone -b 4.5.2 https://github.com/opencv/opencv_contrib.git
+echo "Clone opencv_contrib"
+git clone -b 4.5.2 https://github.com/opencv/opencv_contrib.git >/dev/null
 
 # 删除 opencv_contrib 不是微信二维码识别的 modules
+echo "Remove other modules in opencv_contrib/modules"
 cd opencv_contrib/modules
 ls | grep -P "^((?!wechat_qrcode).)+$" | xargs -d"\n" rm -fr
-ls
 
-cd ${GITHUB_WORKSPACE}
-pwd
+# 创建 build 目录进行编译
+cd ${GITHUB_WORKSPACE} && mkdir build && cd build
+
+# 编译
+python ${GITHUB_WORKSPACE}/opencv/platform/android/build_sdk.py \
+	--extra_modules_path=${GITHUB_WORKSPACE}/opencv_contrib/modules/ \
+	--no_ccache \
+	--build_doc
