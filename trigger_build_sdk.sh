35184e62287efae562b167682f31fed588d02706
chengtao05 <chengtao05@kuaishou.com>
2021-05-13 15:40:09 +0800

    fix : fix trigger script

--- trigger_build_sdk.sh
+++ trigger_build_sdk.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
+
+# fetch all tags
 git fetch
-git push orgin --delete $(git tag -l)
+
+# Delete remote and local tags
+git push origin --delete $(git tag -l)
 git tag -d $(git tag -l)
+
+# trigger
 git tag build-0.0.1
+git push --tags
